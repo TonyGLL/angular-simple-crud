a9e9536183c1c8f5ff9a366e997fdb2d3883face
70NY <53057036+TonyGLL@users.noreply.github.com>
2020-06-17 03:21:41 -0500

    fist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-angular';
+
+  employeeArray: Employee[] = [
+
+    { id: 1, name: 'Ryan', country: 'USA' },
+    { id: 2, name: 'Angelica', country: 'CUB' },
+    { id: 3, name: 'Joe', country: 'SPA' }
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee) {
+
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+
+    if (this.selectedEmployee.id === 0) {
+
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+    this.selectedEmployee = new Employee();
+  }
+
+  delete() {
+
+    if (confirm('Are you sure you want to delete it?')) {
+
+      this.employeeArray = this.employeeArray.filter(x => x !== this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+
+  }
 }
